16efa0be53926eb8c986da7404109ac6fec39ff7
SINEdowskY <kub200105@gmail.com>
2024-03-08 16:10:20 +0100

    methods definition for ModelEvaluator

--- ModelEvaluator.py
+++ ModelEvaluator.py
@@ -1,2 +1,15 @@
 import torch
+from torchvision import transforms
+from PIL import Image
 print(torch.__version__)
+
+
+class Evaluator():
+    def __init__(self, image:Image) -> None:
+        self.image =  image
+
+    def __resize_img(self):
+        pass
+
+    def fit(self):
+        pass
