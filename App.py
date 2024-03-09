aa17d4e9996ed3355168dbad07e08aa21e489704
SINEdowskY <kub200105@gmail.com>
2024-03-09 07:27:08 +0100

    basic frontend

--- App.py
+++ App.py
@@ -2,6 +2,9 @@ import streamlit as st
 from streamlit_drawable_canvas import st_canvas, st_image
 import numpy as np
 from PIL import Image, ImageOps
+from torchvision import transforms
+import torch
+from ModelEvaluator import Evaluator
 
 st.header(
     "LeNet Implementation"
@@ -11,18 +14,38 @@ col1, col2 = st.columns(2)
 
 with col1:
     canva = st_canvas(
-        fill_color="#FFFFF",
+        stroke_color="black",
         height=256,
         width=256
     )
-
+    button_predict = st.button("Predict")
 with col2:
     if canva.image_data is not None:
-        resized = Image.fromarray(canva.image_data)
-        resized = resized.resize((28,28))
-        img_array = np.array(resized)
-        img_array = np.mean(img_array, axis=2, keepdims=True)
-        img_array = img_array / 255.0
-        img_array = np.mean(img_array, axis=2, keepdims=True)
-        st.image(img_array)
-        st.text(f"{img_array.shape}")
+        image_data = canva.image_data
+        image = Image.fromarray(canva.image_data)
+        
+        ev = Evaluator(image=image)
+        st.write("stock image")
+        st.image(ev.image, output_format="PNG")
+        st.write(np.array(ev.image).shape)
+        st.write("preprocessed image")
+        st.image(ev.preprocessed_image)
+        st.write(np.array(ev.preprocessed_image).shape)
+        st.write("tensor image")
+        st.image(ev.tensor_image_view)
+        st.write(np.array(ev.tensor_image_view).shape)
+        if button_predict:
+            classes, probabilities = ev.predict()
+            st.write(f"Result:{classes}")
+            st.write(f"Probs:{probabilities}")
+
+
+    
+with col1:
+    if button_predict:
+        st.dataframe(
+        data={
+            "Classes":range(10),
+            "Probabilities":np.array(probabilities).round(2)*100
+        }
+)
